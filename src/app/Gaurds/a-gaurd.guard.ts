7d2dac0dc3a391c7faea5808ce159581fa16b578
Suman Mitra <sumanmitra0096@gmail.com>
2022-09-09 18:14:00 +0530

    Suman Mitra commit on 09/09/2022

---
+++ src/app/Gaurds/a-gaurd.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AGaurdGuard implements CanActivate {
+  constructor(private auth:AuthService){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(this.auth.IsAdminLoggedIn()){
+        return true;
+   }
+   else{
+        return false;
+   }
+  }
+
+}
